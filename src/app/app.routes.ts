99981d95d96f5c9758c105accb2af50f84266b98
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-04 20:59:08 -0300

    adicionando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Formulario } from './component/formulario/formulario';
+import { HomeComponent } from './component/home-component/home-component';
+import { Menu } from './component/menu/menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'cadastro',
+        component: Formulario
+    }
+];
