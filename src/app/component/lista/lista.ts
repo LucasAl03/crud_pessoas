ac4a4427ca48439e15326bfc2a20b6483d0f2cdc
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-05 19:25:44 -0300

    import de pessoaservice

--- src/app/component/lista/lista.ts
+++ src/app/component/lista/lista.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PessoaService } from '../../services/pessoa-service';
 
 @Component({
   selector: 'app-lista',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './lista.html',
   styleUrl: './lista.css',
 })
-export class Lista {}
+export class Lista {
+  
+}
