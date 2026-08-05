9e0fc82b0aaff206c5fea565f518d9338cdc9a52
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-04 21:43:30 -0300

    pessoaservice importado ao formulario

--- src/app/component/formulario/formulario.ts
+++ src/app/component/formulario/formulario.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { PessoaService } from '../../services/pessoa-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [PessoaService, FormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
-export class Formulario {}
+export class Formulario {
+  nome = ''
+  email = ''
+  cpf = ''
+  dataNascimento = ''
+
+  constructor(private PessoaService: PessoaService){}
+
+  save(){
+    this.PessoaService.adicionar({
+      id: 1,
+      nome: this.nome,
+      email: this.email,
+      cpf: this.cpf,
+      dataNascimento: this.dataNascimento,
+    })
+  }
+}
