5c5b663417de0496db8c8c78e1b4a13a22d2cdf8
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-08-11 14:14:33 -0300

    Codigo para usar a api do IBGE

--- src/app/services/uf-municipios/uf-municipios.ts
+++ src/app/services/uf-municipios/uf-municipios.ts
@@ -1,4 +1,34 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UF } from '../../models/uf';
+import { Municipio } from '../../models/municipio';
 
-@Service()
-export class UfMunicipios {}
+@Injectable({
+  providedIn: 'root',
+})
+export class UfMunicipios {
+
+  constructor(private http: HttpClient) { }
+
+  listarUF(): Observable<UF[]> {
+    //const urlApi = `https://brasilapi.com.br/api/ibge/uf/v1`
+    const urlApi = `https://servicodados.ibge.gov.br/api/v1/localidades/estados`
+    return this.http.get<UF[]>(urlApi)
+  }
+
+  listarMunicipios(uf: string): Observable<Municipio[]> {
+    const urlApi = `https://brasilapi.com.br/api/ibge/municipios/v1/${uf}`
+    return this.http.get<Municipio[]>(urlApi)
+
+  }
+
+  listarMunicipiosIBGE(idUf: number): Observable<Municipio[]> {
+    const urlApi = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${idUf}/municipios`
+
+    return this.http.get<Municipio[]>(urlApi)
+
+  }
+
+
+}
